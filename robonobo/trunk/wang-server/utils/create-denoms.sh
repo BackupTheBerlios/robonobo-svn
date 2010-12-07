f0bc85075702d53e3cf3ee644452777108242bae
macavity <macavity@9d49d850-f87f-0410-bb4a-fc9867aa1288>
2010-12-07 16:35:17 +0000

    Updated create-denoms for wang

--- robonobo/trunk/wang-server/utils/create-denoms.sh
+++ robonobo/trunk/wang-server/utils/create-denoms.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CLASSPATH=../target/classes:../../wang-client/target/classes:../WebContent/WEB-INF/lib/spring.jar:../../common/lib/log4j-1.2.13.jar:../../common/lib/commons-logging-1.0.3.jar:../../common-hibernate/lib/postgresql-8.3-603.jdbc3.jar:../../common/lib/jdom1.1.jar
+CLASSPATH=../target/classes:../../wang-client/target/classes
+for jar in ../../common/lib/*.jar ../../common-hibernate/lib/*.jar ../WebContent/WEB-INF/lib/*.jar
+do
+	CLASSPATH=$CLASSPATH:$jar
+done
 #DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=7000,server=y,suspend=y"
 java $DEBUG -cp $CLASSPATH com.robonobo.wang.server.utils.CreateDenoms $*
 
