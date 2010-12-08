96a8ae62211c44931fb649950db660a263166d1e
macavity <macavity@9d49d850-f87f-0410-bb4a-fc9867aa1288>
2010-12-08 15:03:57 +0000

    Fixing facebook realtime subscription

--- robonobo/trunk/midas-webapp/src/java/com/robonobo/midas/controller/FacebookController.java
+++ robonobo/trunk/midas-webapp/src/java/com/robonobo/midas/controller/FacebookController.java
@@ -19,6 +19,7 @@ import com.robonobo.midas.model.MidasUserConfig;
 import com.robonobo.remote.service.MidasService;
 
 @Controller
+@RequestMapping(value = "/fb-callback")
 public class FacebookController extends BaseController {
 	@Autowired
 	FacebookService facebook;
@@ -26,11 +27,9 @@ public class FacebookController extends BaseController {
 	MidasService midas;
 	ObjectMapper jsonMapper = new ObjectMapper();
 
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/fb-callback")
-	public void facebookCallback(@RequestParam(value = "hub.challenge", required = false) String challenge,
-			@RequestParam("hub.verify_token") String verifyToken, HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+	@RequestMapping(method=RequestMethod.GET)
+	public void facebookVerifyUrl(@RequestParam(value = "hub.challenge", required = false) String challenge,
+			@RequestParam("hub.verify_token") String verifyToken, HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		if (!verifyToken.equals(facebook.getFacebookVerifyTok())) {
 			// Someone is playing silly buggers, maybe?
 			log.error("Facebook called back with invalid verify token " + verifyToken + " (was expecting "
@@ -38,39 +37,39 @@ public class FacebookController extends BaseController {
 			resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
 			return;
 		}
-		// When we first subscribe they send a GET to check the url, after that they send a POST with new data
-		log.info("Facebook callback received with method: " + req.getMethod() + "and params: " + req.getParameterMap());
-		if (req.getMethod().equalsIgnoreCase("POST")) {
-			// Parse our json using the wonderful jackson library
-			Map<String, Object> json;
-			try {
-				json = jsonMapper.readValue(req.getInputStream(), Map.class);
-			} catch (Exception e) {
-				throw new IOException(e);
-			}
-			if (!"user".equals(json.get("object")))
-				throw new IOException("Invalid object in facebook callback: " + json.get("object"));
-			List<Object> changedObjs = (List<Object>) json.get("entry");
-			log.info("Facebook callback received with " + changedObjs.size() + " changed objects");
-			for (Object obj : changedObjs) {
-				Map<String, Object> changedObj = (Map<String, Object>) obj;
-				String fbId = (String) changedObj.get("uid");
-				MidasUserConfig muc = facebook.getUserConfigByFacebookId(fbId);
-				if (muc == null)
-					continue;
-				List<Object> changedFields = (List<Object>) changedObj.get("changed_fields");
-				for (Object field : changedFields) {
-					if ("name".equals(field))
-						facebook.updateFacebookName(fbId, (String) field);
-					else if ("friends".equals(field)) {
-						MidasUser user = midas.getUserById(muc.getUserId());
-						facebook.updateFriends(user, null, muc);
-					}
+		resp.getWriter().print(challenge);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(method=RequestMethod.POST)
+	public void facebookDataChanged(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		// Parse our json using the wonderful jackson library
+		Map<String, Object> json;
+		try {
+			json = jsonMapper.readValue(req.getInputStream(), Map.class);
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+		if (!"user".equals(json.get("object")))
+			throw new IOException("Invalid object in facebook callback: " + json.get("object"));
+		List<Object> changedObjs = (List<Object>) json.get("entry");
+		log.info("Facebook callback received with " + changedObjs.size() + " changed objects");
+		for (Object obj : changedObjs) {
+			Map<String, Object> changedObj = (Map<String, Object>) obj;
+			String fbId = (String) changedObj.get("uid");
+			MidasUserConfig muc = facebook.getUserConfigByFacebookId(fbId);
+			if (muc == null)
+				continue;
+			List<Object> changedFields = (List<Object>) changedObj.get("changed_fields");
+			for (Object field : changedFields) {
+				if ("name".equals(field))
+					facebook.updateFacebookName(fbId, (String) field);
+				else if ("friends".equals(field)) {
+					MidasUser user = midas.getUserById(muc.getUserId());
+					facebook.updateFriends(user, null, muc);
 				}
 			}
 		}
-		// Realtime API spec at http://developers.facebook.com/docs/api/realtime requires us to echo back the challenge,
-		// fuck knows why
-		resp.getWriter().print(challenge);
 	}
 }
