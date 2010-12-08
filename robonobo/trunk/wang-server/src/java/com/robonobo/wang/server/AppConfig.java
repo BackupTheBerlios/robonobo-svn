a3f80d62824c48ea67372b6ffef767f58d47ff1b
macavity <macavity@9d49d850-f87f-0410-bb4a-fc9867aa1288>
2010-12-08 16:21:46 +0000

    Added exception catchers

--- robonobo/trunk/wang-server/src/java/com/robonobo/wang/server/AppConfig.java
+++ robonobo/trunk/wang-server/src/java/com/robonobo/wang/server/AppConfig.java
@@ -2,13 +2,21 @@ package com.robonobo.wang.server;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.ServletContextAware;
 
+import com.robonobo.common.concurrent.SafetyNet;
+import com.robonobo.common.util.ExceptionEvent;
+import com.robonobo.common.util.ExceptionListener;
+
 @Configuration
-public class AppConfig implements ServletContextAware {
+public class AppConfig implements ServletContextAware, InitializingBean {
 	private ServletContext sc;
+	Log log = LogFactory.getLog(getClass());
 	
 	@Bean
 	public RemoteWangService getRemoteWang() throws Exception {
@@ -23,5 +31,14 @@ public class AppConfig implements ServletContextAware {
 	@Override
 	public void setServletContext(ServletContext sc) {
 		this.sc = sc;
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		SafetyNet.addListener(new ExceptionListener() {
+			public void onException(ExceptionEvent e) {
+				log.error("Uncaught exception", e.getException());
+			}
+		});
 	}
 }
