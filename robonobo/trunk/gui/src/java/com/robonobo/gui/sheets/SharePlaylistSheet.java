1c949660d04cfc0841ee832222074b6f00ae6e57
macavity <macavity@9d49d850-f87f-0410-bb4a-fc9867aa1288>
2011-02-25 18:33:46 +0000

    SharePlaylistSheet now only enables when a friend/email is selected

--- robonobo/trunk/gui/src/java/com/robonobo/gui/sheets/SharePlaylistSheet.java
+++ robonobo/trunk/gui/src/java/com/robonobo/gui/sheets/SharePlaylistSheet.java
@@ -7,8 +7,11 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.*;
 import java.util.*;
+import java.util.regex.Pattern;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -47,7 +50,6 @@ public class SharePlaylistSheet extends Sheet {
 		add(titleLbl, "1,1,3,1");
 		RLabel exFriendLbl = new RLabel12("Existing friends:");
 		add(exFriendLbl, "1,3,l,t");
-
 		Vector<UserWrapper> friends = new Vector<UserWrapper>();
 		for (Long friendId : control.getMyUser().getFriendIds()) {
 			User user = control.getUser(friendId);
@@ -58,25 +60,28 @@ public class SharePlaylistSheet extends Sheet {
 		friendList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		friendList.setSelectionModel(new MySelectionModel(friendList.getModel()));
 		friendList.setCellRenderer(new MyCellRenderer());
+		friendList.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				shareBtn.setEnabled(targetSelected());
+			}
+		});
 		add(new JScrollPane(friendList), "3,3");
 
 		RLabel newFriendLbl = new RLabel12("New friends:");
 		add(newFriendLbl, "1,5");
 		emailField = new RTextField(DEFAULT_EMAILS);
-		// Blank on focus
-		emailField.addFocusListener(new FocusAdapter() {
-			public void focusGained(FocusEvent e) {
-				if (DEFAULT_EMAILS.equals(emailField.getText()))
-					emailField.setText("");
+		emailField.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				shareBtn.setEnabled(targetSelected());
 			}
 		});
 		add(emailField, "3,5");
 
-		int invitesLeft = control.getMyUser().getInvitesLeft();
-		if (invitesLeft <= 0)
-			emailField.setEnabled(false);
-		RLabel inviteLbl = new RLabel12(numItems(invitesLeft, "invite") + " left");
-		add(inviteLbl, "1,7");
+//		int invitesLeft = control.getMyUser().getInvitesLeft();
+//		if (invitesLeft <= 0)
+//			emailField.setEnabled(false);
+//		RLabel inviteLbl = new RLabel12(numItems(invitesLeft, "invite") + " left");
+//		add(inviteLbl, "1,7");
 
 		add(new ButtonPanel(), "3,7,r,t");
 	}
@@ -90,6 +95,13 @@ public class SharePlaylistSheet extends Sheet {
 	@Override
 	public JButton defaultButton() {
 		return shareBtn;
+	}
+	
+	private boolean targetSelected() {
+		if(friendList.getSelectedIndices().length > 0)
+			return true;
+		// Could bugger about with regexes here, but I don't think it's worth it
+		return (emailField.getText().length() > 0 && !emailField.getText().equals(DEFAULT_EMAILS)) ;
 	}
 	
 	private class ButtonPanel extends JPanel {
@@ -127,6 +139,7 @@ public class SharePlaylistSheet extends Sheet {
 				}
 			});
 			add(shareBtn);
+			shareBtn.setEnabled(targetSelected());
 			Dimension fillerD = new Dimension(20, 1);
 			add(new Box.Filler(fillerD, fillerD, fillerD));
 			cancelBtn = new RRedGlassButton("CANCEL");
